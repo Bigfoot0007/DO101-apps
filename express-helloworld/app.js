f9634756b2bdb2ad80015b2a1e94cd3414978f33
xyguo <xyguo@cn.ibm.com>
2020-04-01 14:16:52 +0800

    version2 testing

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello World!,this is the version 2\n');
 });
 
 app.get('/mars', function (req, res) {
